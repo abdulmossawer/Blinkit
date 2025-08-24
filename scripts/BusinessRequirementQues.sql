e905cab6f6e1f256ba17f11ea96043719aa09209
Abdul Mossawer <142085179+abdulmossawer@users.noreply.github.com>
2025-08-24 18:08:13 +0530

    Update BusinessRequirementQues.sql

--- scripts/BusinessRequirementQues.sql
+++ scripts/BusinessRequirementQues.sql
@@ -7,7 +7,7 @@ SELECT * FROM blinkit_data;
 -- Total Sales
 CREATE VIEW Total_Sales AS
 SELECT
-	CAST(SUM(Total_Sales) / 1000000 AS DECIMAL(10, 2) ) AS Total_Sales_Millions
+	CAST(SUM(Total_Sales) / 1000000 AS DECIMAL(10, 2)) AS Total_Sales_Millions
 FROM blinkit_data;
 
 -- Average Sales
@@ -27,3 +27,55 @@ CREATE VIEW Avg_Rating AS
 SELECT
 	CAST(AVG(Rating) AS DECIMAL(10, 1)) AS Avg_Rating
 FROM blinkit_data;
+
+
+-- Granular Requirements
+
+--Total Sales by Fat Content
+CREATE VIEW Total_Sales_by_Fat_Content AS
+SELECT
+	Item_Fat_Content,
+	CAST(SUM(Total_Sales) AS DECIMAL(10, 2)) AS Total_Sales,
+	CAST(AVG(Total_Sales) AS DECIMAL(10, 2) ) AS Avg_Sales,
+	COUNT(*) AS No_Of_Items,
+	CAST(AVG(Rating) AS DECIMAL(10,1)) AS Avg_Rating
+FROM blinkit_data
+GROUP BY Item_Fat_Content;
+
+-- Total Sales by Item Type
+CREATE VIEW Total_Sales_by_Item_Type AS
+SELECT 
+	Item_Type,
+	CAST(SUM(Total_Sales) AS DECIMAL(10,2))AS Total_Sales,
+	CAST(AVG(Total_Sales) AS DECIMAL(10, 2) ) AS Avg_Sales,
+	COUNT(*) AS No_Of_Items,
+	CAST(AVG(Rating) AS DECIMAL(10,1)) AS Avg_Rating
+FROM blinkit_data
+GROUP BY Item_Type;
+
+-- Fat Content by Outlet for Total Sales
+CREATE VIEW Fat_Content_by_Outlet_for_Total_Sales AS
+SELECT Outlet_Location_Type, 
+       ISNULL([Low Fat], 0) AS Low_Fat, 
+       ISNULL([Regular], 0) AS Regular
+FROM 
+(
+    SELECT Outlet_Location_Type, Item_Fat_Content, 
+           CAST(SUM(Total_Sales) AS DECIMAL(10,2)) AS Total_Sales
+    FROM blinkit_data
+    GROUP BY Outlet_Location_Type, Item_Fat_Content
+) AS SourceTable
+PIVOT 
+(
+    SUM(Total_Sales)
+    FOR Item_Fat_Content IN ([Low Fat], [Regular])
+) AS PivotTable
+
+-- Total Sales by Outlet Establishment
+CREATE VIEW Total_Sales_by_Outlet_Establishment AS
+SELECT 
+	Outlet_Establishment_Year,
+	CAST(SUM(Total_Sales) AS DECIMAL(10,2)) AS Total_Sales
+FROM blinkit_data
+GROUP BY Outlet_Establishment_Year;
+
